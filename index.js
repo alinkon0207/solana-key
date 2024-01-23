2bb06c45883dc128fc643885736b817293b15166
ilesoviy <ilisovij861@gmail.com>
2024-01-23 14:25:25 +0300

    Deconversion done

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import bs58 from "bs58";
 import { PublicKey, Keypair } from "@solana/web3.js";
 import fs from 'fs';
 
-const userKeypair = Keypair.fromSecretKey(bs58.decode(""));
-
-const main = () => {
+const convert = () => {
+  const userKeypair = Keypair.fromSecretKey(bs58.decode(""));
   console.log(userKeypair.publicKey.toBase58());
 
   let str = "[";
@@ -18,7 +17,27 @@ const main = () => {
   }
   str += ']';
 
-  fs.writeFileSync("deploy-keypair.json", str);
+  fs.writeFileSync("keypair.json", str);
 }
 
-main();
+const deconvert = () => {
+  let str = fs.readFileSync("keypair.json")?.toString();
+  if (str[0] != '[' || str[str.length - 1] != ']') {
+    console.log("Invalid keypair");
+    return;
+  }
+
+  str = str.substring(1, str.length - 1);
+  const splitStr = str.split(',');
+  let secretKey = new Array(64);
+  for (let i = 0; i < splitStr.length; i++) {
+    const num = Number(splitStr[i]);
+    secretKey[i] = num;
+  }
+  
+  const privKey = bs58.encode(secretKey);
+  console.log('privKey:', privKey);
+}
+
+// convert();
+deconvert();
